8a682b94654723d3940998fef9aba0c6852837bd
Waithera <stacywaithera@DESKTOP-E0D09HN>
2025-03-06 14:59:08 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(num1, num2) {
+    console.log('divide ${num1}');
+    console.log('divide ${num2}');
+    return num1 / num2;
+}
+const add = (num1, num2) => {
+    console.log('add ${num1}');
+    console.log('add ${num2}');
+    return num1 + num2;
+}
+const square = (x) => {
+    return x * x;
+}
